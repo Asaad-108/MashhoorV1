a4d34fa4733987d69d8b70148f3ff836607f9901
Asaad-108 <asadasghar836@gmail.com>
2026-06-06 02:02:38 +0500

    logo fixes

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,10 +1,11 @@
 import '../App.css';
+import logo from '../assets/mashhoorLogo.svg';
 
 function Logo() {
     return (
         <>
             <div className="img bg-linear-to-br from-blue-500 to-purple-500 flex justify-center rounded-xl w-9 h-9 p-1">
-                <img src="src/assets/mashhoorLogo.svg" alt="" />
+                <img src={logo} alt="Mashhoor Logo" />
             </div>
         </>
     )
