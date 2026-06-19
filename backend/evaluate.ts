566d04f7e9ff402526b62639c5214e6857ad0fd3
hassan0981 <hass.javed25@gmail.com>
2026-06-20 01:44:30 +0500

    Cleaned

--- backend/evaluate.ts
+++
@@ -1,7 +0,0 @@
-import { predictCampaignROIWithML, trustRegressionModel, trustBenchmarkDataset } from "./src/services/mlService";
-
-const roiResult = predictCampaignROIWithML(10000, 5, 80, 20000, 2500);
-console.log("ROI Metrics:", roiResult.aiModelMetrics);
-
-const trustMetrics = trustRegressionModel.evaluate(trustBenchmarkDataset);
-console.log("Trust Metrics:", trustMetrics);
